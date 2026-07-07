ce785eaf3c53932aafeca4e4691b8d92dc3def1b
vothnha26 <vothanhnha26@gmail.com>
2026-07-07 21:23:42 +0700

    fix(polyfill): correct Unicode normalization form to NFD

--- backend/src/utils/polyfill.js
+++ backend/src/utils/polyfill.js
@@ -1,5 +1,5 @@
 if (!String.prototype.toWellFormed) {
   String.prototype.toWellFormed = function () {
-    return this.normalize('D').replace(/[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?<![\uD800-\uDBFF])[\uDC00-\uDFFF]/g, '\uFFFD');
+    return this.normalize('NFD').replace(/[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?<![\uD800-\uDBFF])[\uDC00-\uDFFF]/g, '\uFFFD');
   };
 }
